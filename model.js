7798a3ccc04da6f748fb4309aa56d7491eac093d
zhufengnodejs <zhufengnodejs@126.com>
2017-01-11 17:23:13 +0800

    列表页

---
+++ model.js
@@ -0,0 +1,15 @@
+let mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1/ng-book');
+//定义模型骨架
+let BookSchema = new mongoose.Schema({
+    name:String,
+    price:Number,
+    src:String
+});
+//定义并导出模型
+exports.Book = mongoose.model('Book',BookSchema);
+exports.Book.create({
+    name:'node.js实战',
+    price:80,
+    src:'http://img14.360buyimg.com/n0/g16/M00/07/12/rBEbRlN1yQoIAAAAAAMXeVdM1lEAABejgCMrnIAAxeR712.jpg'
+});
--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 let express = require('express');
+let path = require('path');
 let app = express();
-app.get('/',function(req,res){
-  res.sendFile('./app/index.html',{root:__dirname})
+app.use(express.static(path.resolve('public')));
+app.use(express.static(path.resolve('app')));
+let Book = require('./model').Book;
+app.get('/', function (req, res) {
+    res.sendFile('./app/index.html', {root: __dirname})
 });
+    //当客户端以GET方法访问/books路径的时候
+app.route('/books')
+    .get(function (req, res) {
+        Book.find({}, function (err, books) {
+            res.send(books);
+        })
+    //当客户端以POST方法访问/books路径的时候
+    }).post(function (req, res) {
+
+    })
+
 app.listen(8080);
